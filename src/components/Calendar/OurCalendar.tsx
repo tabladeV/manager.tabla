1f17aa00b887583553ca5bff4196a0f144e5fd71
Al Hassan Zakriti <ALHASSAN.ZAKRITI-ETU@etu.univh2c.ma>
2024-08-22 18:39:40 +0100

    issues fixed

--- src/components/Calendar/OurCalendar.tsx
+++ src/components/Calendar/OurCalendar.tsx
@@ -9,27 +9,27 @@ import {
   isToday,
   parse,
   startOfToday,
-  add
+  add,
 } from 'date-fns';
 
 const colStartClasses = [
-  '', 'col-start-2', 'col-start-3', 'col-start-4', 'col-start-5', 'col-start-6', 'col-start-7'
+  '', 'col-start-2', 'col-start-3', 'col-start-4', 'col-start-5', 'col-start-6', 'col-start-7',
 ];
 
 interface OurCalendarProps {
   onClick?: (day: Date) => void;
 }
 
-const OurCalendar: React.FC<OurCalendarProps> = (props) => {
+const OurCalendar: React.FC<OurCalendarProps> = ({ onClick }) => {
   const today = startOfToday();
+  const [selectedDay, setSelectedDay] = useState<Date>(today);
+  const [currentMonth, setCurrentMonth] = useState<Date>(today);
 
-  const classNames = (...classes: (string | undefined)[]) => {
+  const classNames = (...classes: (string | undefined)[]): string => {
     return classes.filter(Boolean).join(' ');
   };
 
-  const [selectedDay, setSelectedDay] = useState<Date>(today);
-  const [currentMonth, setCurrentMonth] = useState<string>(format(today, 'MMM-yyyy'));
-  const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date());
+  const firstDayCurrentMonth = parse(format(currentMonth, 'MMM-yyyy'), 'MMM-yyyy', new Date());
 
   const days = eachDayOfInterval({
     start: firstDayCurrentMonth,
@@ -37,44 +37,48 @@ const OurCalendar: React.FC<OurCalendarProps> = (props) => {
   });
 
   const previousMonth = () => {
-    const firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
+    setCurrentMonth(add(currentMonth, { months: -1 }));
   };
 
   const nextMonth = () => {
-    const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
-    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
+    setCurrentMonth(add(currentMonth, { months: 1 }));
   };
 
   const selectingDate = (day: Date) => {
     setSelectedDay(day);
-    if (props.onClick) {
-      props.onClick(day); // Call the onClick function and pass the day as an argument
+    if (onClick) {
+      onClick(day); // Call the onClick function and pass the day as an argument
     }
   };
 
   return (
-    <div className='p-[1em] w-full h-full mx-auto'>
-      <div className='text-[20px] items-center mb-2 flex justify-between'>
-        <div className='font-bold'>{format(firstDayCurrentMonth, 'MMMM yyyy')}</div>
-        <div className='flex'>
-          <button onClick={previousMonth} className='hover:bg-[#3333330a] transition duration-500 w-[35px] h-[35px] flex justify-center items-center rounded-[100%]'>
-            {/* SVG for previous month */}
+    <div className="p-[1em] w-full h-full mx-auto">
+      <div className="text-[20px] items-center mb-2 flex justify-between">
+        <div className="font-bold">{format(currentMonth, 'MMMM yyyy')}</div>
+        <div className="flex">
+          <button
+            onClick={previousMonth}
+            className="hover:bg-[#3333330a] transition duration-500 w-[35px] h-[35px] flex justify-center items-center rounded-[100%]"
+          >
             {'<'}
           </button>
-          <button onClick={nextMonth} className='hover:bg-[#3333330a] transition duration-500 w-[35px] h-[35px] flex justify-center items-center rounded-[100%]'>
-            {/* SVG for next month */}
+          <button
+            onClick={nextMonth}
+            className="hover:bg-[#3333330a] transition duration-500 w-[35px] h-[35px] flex justify-center items-center rounded-[100%]"
+          >
             {'>'}
           </button>
         </div>
       </div>
-      <div className='mx-auto'>
-        <div className='grid mx-auto grid-cols-7'>
+      <div className="mx-auto">
+        <div className="grid mx-auto grid-cols-7">
           {['M', 'T', 'W', 'T', 'F', 'S', 'S'].map((day, index) => (
-            <button key={index} className='font-bold w-[30px] cursor-default rounded-[6px] h-[30px]'>{day}</button>
+            <button key={index} className="font-bold w-[30px] cursor-default rounded-[6px] h-[30px]">
+              {day}
+            </button>
           ))}
         </div>
-        <div className='mx-auto grid grid-cols-7 justify-around'>
+        <div className="mx-auto grid grid-cols-7 justify-around">
           {days.map((day, dayIdx) => (
             <div key={day.toString()} className={classNames(dayIdx === 0 && colStartClasses[getDay(day)], 'py-1.5')}>
               <button
@@ -83,8 +87,8 @@ const OurCalendar: React.FC<OurCalendarProps> = (props) => {
                 className={classNames(
                   isEqual(day, selectedDay) && 'text-white',
                   !isEqual(day, selectedDay) && isToday(day) && 'text-[#70ae29] font-bold',
-                  !isEqual(day, selectedDay) && !isToday(day) && isSameMonth(day, firstDayCurrentMonth) && 'text-gray-900',
-                  !isEqual(day, selectedDay) && !isToday(day) && !isSameMonth(day, firstDayCurrentMonth) && 'text-gray-400',
+                  !isEqual(day, selectedDay) && !isToday(day) && isSameMonth(day, currentMonth) && 'text-gray-900',
+                  !isEqual(day, selectedDay) && !isToday(day) && !isSameMonth(day, currentMonth) && 'text-gray-400',
                   isEqual(day, selectedDay) && isToday(day) && 'bg-greentheme font-bold',
                   isEqual(day, selectedDay) && !isToday(day) && 'bg-gray-900',
                   !isEqual(day, selectedDay) && 'hover:bg-gray-200',
